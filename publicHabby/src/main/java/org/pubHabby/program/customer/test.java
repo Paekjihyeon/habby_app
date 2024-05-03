bb3ffb0422ab5e4a5ee3521f8072b232400d41f8
DESKTOP-PDOLIU4\admin <skysun@g1ncom.co.kr>
2024-05-03 16:56:49 +0900

    수정

--- publicHabby/src/main/java/org/pubHabby/program/customer/test.java
+++ publicHabby/src/main/java/org/pubHabby/program/customer/test.java
@@ -1,10 +1,12 @@
 package org.pubHabby.program.customer;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class test {
         @GetMapping("/api/hello")
--- publicHabby/src/main/reactRepo/pubHabby/src/App.js
+++ publicHabby/src/main/reactRepo/pubHabby/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [str, setStr] = useState("");
 
   useEffect(() => {
   axios
-    .get("/api/hello")
-    .then((response) => {console.log(response)})
-    .catch((error) => console.log("???" + error));
+    .get("http://localhost:8081/api/hello")
+    .then((response) => {
+      setStr(response.data);
+    })
+    .catch((error) => console.log(error));
   }, []);
 
-  return <div>백엔드에서 가져온 데이터입니다 </div>;
+  return <div>백엔드에서 가져온 데이터입니다 : {str}</div>;
 }
 
 export default App;
